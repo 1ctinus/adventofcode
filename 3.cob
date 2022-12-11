3035e8045e33c4c4d5dddd9ca71bba30fb9e538c
Ict <72636637+1ctinus@users.noreply.github.com>
2022-12-11 13:18:49 -0600

    shrunk cobol more

--- 3.cob
+++ 3.cob
@@ -4,9 +4,9 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT input-file ASSIGN TO "3.txt"
-               ORGANIZATION LINE SEQUENTIAL
-               FILE STATUS input-file-status.
+       SELECT input-file ASSIGN TO "3.txt"
+           ORGANIZATION LINE SEQUENTIAL
+           FILE STATUS input-file-status.
 
        DATA DIVISION.
        FILE SECTION.
@@ -16,7 +16,6 @@
        01  input-file-status PIC 99.
            88  file-is-ok    VALUE 0.
            88  end-of-file   VALUE 10.
-
        01  line-count        PIC 9(6).
        01 line1 PIC X(256).
        01 line2 PIC X(256).
@@ -34,8 +33,8 @@
        main.
            OPEN INPUT input-file
            IF NOT file-is-ok
-               DISPLAY "The file could not be opened."
-               GOBACK
+             DISPLAY "The file could not be opened."
+GOBACK
            END-IF
 
            PERFORM VARYING line-count FROM 1 BY 1 UNTIL end-of-file
@@ -45,46 +44,36 @@
                  MOVE FUNCTION TRIM(input-record) TO line1
                END-IF
                IF h = 2
-                MOVE FUNCTION TRIM(input-record) TO line2
+                 MOVE FUNCTION TRIM(input-record) TO line2
                END-IF
                IF h = 3
                  MOVE 0 TO h
                  MOVE FUNCTION TRIM(input-record) TO line3 
+                 PERFORM VARYING i FROM 1 BY 1 UNTIL i > 
+                   FUNCTION BYTE-LENGTH(FUNCTION TRIM(line1))
+                   PERFORM VARYING j FROM 1 BY 1 UNTIL j > 
+                     FUNCTION BYTE-LENGTH(FUNCTION TRIM(line2))
+                     PERFORM VARYING k FROM 1 BY 1 UNTIL k > 
+                       FUNCTION BYTE-LENGTH(FUNCTION TRIM(line3))
+                       IF line1(i:1) = line2(j:1)
+                         IF line1(i:1) = line3(k:1)
+                           MOVE line2(j:1) TO chosenone
+                         END-IF
+                       END-IF
+
+                     END-PERFORM
+                   END-PERFORM
+                 END-PERFORM
                  MOVE 1 TO i
-                 PERFORM loop UNTIL i > FUNCTION BYTE-LENGTH(FUNCTION
-                 TRIM(line1))
-                 MOVE 1 TO i
-                 PERFORM converttoout UNTIL i = 53
+                 PERFORM VARYING i FROM 1 BY 1 UNTIL i = 53
+                   IF chosenone = lookup(i:1)
+                     ADD i to out
+                   END-IF
+                 END-PERFORM  
                  DISPLAY out
                END-IF
-            ADD 1 TO h
+               ADD 1 TO h
            END-PERFORM
            CLOSE input-file
-           GOBACK
-           .
-        loop.
-           PERFORM anotherloop UNTIL j> FUNCTION BYTE-LENGTH(FUNCTION
-           TRIM(line2))
-           ADD 1 to i
-           MOVE 1 TO j
-           .
-        anotherloop.
-           PERFORM checkifsame UNTIL k> FUNCTION BYTE-LENGTH(FUNCTION
-           TRIM(line3))
-           MOVE 1 to k
-           ADD 1 to j
-           .
-       checkifsame.
-           IF line1(i:1) = line2(j:1)
-           IF line1(i:1) = line3(k:1)
-             MOVE line2(j:1) TO chosenone
-          END-IF
-           END-IF
-          ADD 1 to k
-       .
-       converttoout.
-           IF chosenone = lookup(i:1)
-             ADD i to out
-           END-IF
-           ADD 1 to i
+GOBACK
            .
